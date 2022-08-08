cd0aec617ed1cd035e366bb31412214a31c09f87
rgudla3 <rgudla3@gatech.edu>
2022-08-07 21:47:02 -0400

    made header page responsive

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -8,26 +8,34 @@ import reshma_intro from '../../assets/ReshmaPic.png';
 
 const Home = () => {
   return (
-    <><div className="hi-there">Hi there! I’m Reshma.</div>
-    <div className="fairy-lights">
+    <div className="home__header" id="home">
+      
+      <div className="home__header-content">
+      
+      <div className="fairy-lights">
       <img src={fairy} />
       </div>
-      <div className="welcome-msg">Welcome to my website.</div>
+      
+      <h1 className="hi-there">Hi there! I’m Reshma.</h1>
+      <h2 className="welcome-msg">Welcome to my website.</h2>
       <div className="line-separater" />
-      <div className="linkedin">
-      <a href="https://linkedin.com/in/reshma-gudla-930217220/"><img src={linkedin}/></a>
+      <div className="contact">
+        <div className="linkedin">
+        <a href="https://linkedin.com/in/reshma-gudla-930217220/"><img src={linkedin}/></a>
+        </div>
+        <div className="github">
+        <a href="https://github.com/rgudla3"><img src={github}/></a>
+        </div>
+        <div className="email">
+        <a href="mailto:reshmagudla03@gmail.com"><img src={email}/></a>
+        </div>
       </div>
-      <div className="github">
-      <a href="https://github.com/rgudla3"><img src={github}/></a>
+      
       </div>
-      <div className="email">
-      <a href="mailto:reshmagudla03@gmail.com"><img src={email}/></a>
-      </div>
-
       <div className="reshma-intro-pic">
         <img src={reshma_intro} />
       </div>
-    </>
+    </div>
   )
 }
 
